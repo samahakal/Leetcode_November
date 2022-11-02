26ebb458acfb030a3bd7b60c341707f050a3eff7
samahakal <91861509+samahakal@users.noreply.github.com>
2022-11-02 10:05:05 +0530

    433. Minimum Genetic Mutation

--- November_2.cpp
+++ November_2.cpp
@@ -2,6 +2,7 @@
 // https://leetcode.com/problems/minimum-genetic-mutation/description/
 
 
+Code : DFS
 class Solution {
 public:
 
@@ -18,7 +19,7 @@ public:
         return cnt;
     }
 
-    int solve(unordered_map<string,int>&mp,string st,string en){
+    int solve(unordered_map<string,int>mp,string st,string en){
         if(st == en){
             return 0;
         }
@@ -33,7 +34,6 @@ public:
             if(diff(it.first,st) == 1){
                 mp[it.first]--;
                 ans = min(ans,1 + solve(mp,it.first,en));
-                mp[it.first]++;
             }
         }
         return ans;
@@ -52,5 +52,55 @@ public:
         }
         
         return mut;
+    }
+};
+
+
+Code : BFS 
+class Solution {
+public:
+
+   
+
+    int minMutation(string start, string end, vector<string>& bank) {
+
+        
+        unordered_set<string> dict(bank.begin(),bank.end());
+
+        // base case
+        if(dict.count(end) == 0){
+            return -1;
+        }
+
+        int cnt = 0;
+        queue<string>q;
+        q.push(start);
+        while(!q.empty()){
+            int n = q.size();
+            while(n--){
+                string temp = q.front();
+                q.pop();
+
+                if(temp == end){
+                    return cnt;
+                }
+                dict.erase(temp);
+
+                for(int i=0;i<8;i++){
+                    char ch = temp[i];
+                    for(char c : "ACGT"){
+                        temp[i] = c;
+                        if(dict.count(temp)){
+                            q.push(temp);
+                        }
+                    }
+                    temp[i] = ch;
+                }                
+
+            }
+            cnt++;
+        }
+        
+        return -1;
     }
 };
